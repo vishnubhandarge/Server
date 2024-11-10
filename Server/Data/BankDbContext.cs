5afcd798ef0853247dfa85d81b498e4ee33f583f
vishnubhandarge <vishnubhandarge@icloud.com>
2024-11-10 16:02:08 +0530

    Update BankDbContext.cs

--- Server/Data/BankDbContext.cs
+++ Server/Data/BankDbContext.cs
@@ -10,26 +10,3 @@ namespace Server.Data
     }
    
 }
-//using Microsoft.EntityFrameworkCore;
-//using Server.Models;
-
-//namespace Server.Data
-//{
-//    public class BankDbContext : DbContext
-//    {
-//        public BankDbContext(DbContextOptions<BankDbContext> options) : base(options)
-//        {
-//        }
-
-//        public DbSet<Customer> Customers { get; set; }
-//        public DbSet<Card> Cards { get; set; }
-
-//        protected override void OnModelCreating(ModelBuilder modelBuilder) 
-//        {
-//            modelBuilder.Entity<Customer>().HasMany(c => c.Cards)
-//                .WithOne(o => o.customer)
-//                .HasForeignKey(o => o.AccountNumber); 
-//            base.OnModelCreating(modelBuilder);
-//        }
-//    }
-//}
